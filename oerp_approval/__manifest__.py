3b908ccd3adbddab86682211a32d1dcffc7edb74
刘福龙 <qq65long@126.com>
2020-08-17 18:17:59 +0800

    [ADD]添加审批模块

---
+++ oerp_approval/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "海尔婴童 | 店铺",
+
+    'description': """
+        店铺管理界面
+    """,
+
+    'author': "LFL",
+    'website': "http://www.haierbaby.com",
+
+    'category': '',
+    'version': '1.0',
+    'depends': ['base', 'mail'],
+    'data': [
+        # 'security/shop_security.xml',
+        'security/ir.model.access.csv',
+        # 'views/shop_views.xml',
+        # 'views/shop_type_views.xml',
+        # 'data/ir_sequence_data.xml',
+    ],
+    'installable': True,
+    'auto_install': False
+}
