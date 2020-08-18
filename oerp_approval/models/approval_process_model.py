3154fdce4115ad581895f895755afe224b50029a
刘福龙 <qq65long@126.com>
2020-08-18 13:04:23 +0800

    [IMP]添加抄送人和审批人

--- oerp_approval/models/approval_process_model.py
+++ oerp_approval/models/approval_process_model.py
@@ -18,9 +18,11 @@ class ApprovalProcessConfig(models.Model):
                                  default=lambda self: self.env.user.company_id, track_visibility='onchange')
     active = fields.Boolean(string='有效', default=True)
     approve_type = fields.Selection(string="审批类型", selection=[('ordinal', '依次审批'), ('complicated', '会签/或签')])
-    # simple_approve_user_ids = fields.Many2many('res.users', 'custom_approve_res_users_rel', string='审批列表')
+    recv_user_ids = fields.Many2many('res.users', 'custom_approve_recv_users_rel', string='抄送人列表')
     # complicated_approve_user_ids = fields.One2many('custom.approve.res.users.rel', 'custom_approve_id', string='审批列表')
     approve_user_ids = fields.One2many('custom.approve.node.line', 'custom_approve_id', string='审批列表')
+    cc_user_ids = fields.Many2many('res.users', 'custom_approve_recv_users_rel', string='抄送人')
+    cc_type = fields.Selection(string="抄送时间", selection=[('START', '审批开始'), ('FINISH', '审批结束'), ('START_FINISH', '开始和结束')], default='FINISH')
 
     # _sql_constraints = [
     # ]
@@ -32,9 +34,18 @@ class CustomApproveResUsersRel(models.Model):
     _description = '自定义审批节点'
 
     custom_approve_id = fields.Many2one('custom.approve.process.config', string='自定义审批id', required=True)
+    group_id = fields.Many2one('res.groups', string="适用权限组")
     user_ids = fields.Many2many('res.users', 'custom_approval_user_list_rel', string="审批人")
-    approval_type = fields.Selection(string="审批类型", selection=[('AND', '会签'), ('OR', '或签'), ('NONE', '单人')],
-                                     required=True, default='NONE')
+    approval_type = fields.Selection(string="审批类型", selection=[('AND', '会签'), ('OR', '或签'), ('ONE', '单人')],
+                                     required=True, default='ONE')
 
     # _sql_constraints = [
     # ]
+
+    @api.onchange('group_id')
+    def onchange_update_approve_users(self):
+        print('--------------', self)
+        for record in self:
+            print(record)
+            if record.group_id:
+                record.user_ids = record.group_id.users
