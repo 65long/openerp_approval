22bc02131f1e857692bc90ffce3ebc21f42a85de
刘福龙 <qq65long@126.com>
2020-08-19 09:07:56 +0800

    [IMP]添加审批明细和模型 按钮等

--- oerp_approval/models/approval_process_model.py
+++ oerp_approval/models/approval_process_model.py
@@ -1,6 +1,7 @@
 # encoding: utf-8
 
 import logging
+from lxml import etree
 from odoo import models, fields, api
 from odoo.exceptions import UserError, Warning
 
@@ -18,7 +19,6 @@ class ApprovalProcessConfig(models.Model):
                                  default=lambda self: self.env.user.company_id, track_visibility='onchange')
     active = fields.Boolean(string='有效', default=True)
     approve_type = fields.Selection(string="审批类型", selection=[('ordinal', '依次审批'), ('complicated', '会签/或签')])
-    recv_user_ids = fields.Many2many('res.users', 'custom_approve_recv_users_rel', string='抄送人列表')
     # complicated_approve_user_ids = fields.One2many('custom.approve.res.users.rel', 'custom_approve_id', string='审批列表')
     approve_user_ids = fields.One2many('custom.approve.node.line', 'custom_approve_id', string='审批列表')
     cc_user_ids = fields.Many2many('res.users', 'custom_approve_recv_users_rel', string='抄送人')
@@ -26,6 +26,29 @@ class ApprovalProcessConfig(models.Model):
 
     # _sql_constraints = [
     # ]
+
+    @api.onchange('oa_model_id')
+    def onchange_model_id(self):
+        """
+        根据选择的模型读取模型动作按钮
+        :return:
+        """
+        for rec in self:
+            if rec.oa_model_id:
+                model_id = rec.oa_model_id
+                print(dir(model_id))
+                result = self.env[model_id.model].fields_view_get()
+                root = etree.fromstring(result['arch'])
+                for item in root.xpath("//header/button"):
+                    domain = [('model_id', '=', model_id.id), ('function', '=', item.get('name'))]
+                    model_buts = self.env['custom.approve.model.button'].sudo().search(domain)
+                    if not model_buts:
+                        self.env['custom.approve.model.button'].create({
+                            'model_id': model_id.id,
+                            'name': item.get('string'),
+                            'function': item.get('name'),
+                            'modifiers': item.get('modifiers'),
+                        })
 
 
 class CustomApproveResUsersRel(models.Model):
@@ -38,14 +61,28 @@ class CustomApproveResUsersRel(models.Model):
     user_ids = fields.Many2many('res.users', 'custom_approval_user_list_rel', string="审批人")
     approval_type = fields.Selection(string="审批类型", selection=[('AND', '会签'), ('OR', '或签'), ('ONE', '单人')],
                                      required=True, default='ONE')
+    view_button_id = fields.Many2one('custom.approve.model.button', string="模型按钮")
 
     # _sql_constraints = [
     # ]
 
     @api.onchange('group_id')
     def onchange_update_approve_users(self):
-        print('--------------', self)
         for record in self:
-            print(record)
             if record.group_id:
                 record.user_ids = record.group_id.users
+
+
+class DingDingApprovalButton(models.Model):
+    _name = 'custom.approve.model.button'
+    _description = '自定义审批流模型按钮'
+    _rec_name = 'name'
+
+    model_id = fields.Many2one('ir.model', string='模型', index=True)
+    model_model = fields.Char(string='模型名', related='model_id.model', store=True, index=True)
+    name = fields.Char(string="按钮名称", index=True)
+    function = fields.Char(string='按钮方法', index=True)
+    modifiers = fields.Char(string="按钮属性值")
+
+    def name_get(self):
+        return [(rec.id, "%s:%s" % (rec.model_id.name, rec.name)) for rec in self]
